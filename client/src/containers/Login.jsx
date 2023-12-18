cdb47905f38a844df7ed119f436c53d1e706e541
MADALIN_VOICULESCU <voiculescu.madalin1999@gmail.com~>
2023-12-18 16:03:27 +0200

    google auth frontend,express start

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -5,11 +5,31 @@ import {FaEnvelope, FaLock, FcGoogle} from "../assets/icons";
 import {motion} from "framer-motion";
 import {buttonClick} from "../animations";
 
+import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+import {app} from "../config/firebase.config";
+
 function Login() {
     const [userEmail, setUserEmail] = useState("");
     const [isSignUp, setIsSignUp] = useState(false);
     const [password, setPassword] = useState("");
     const [confirm_password, setConfirm_password] = useState("");
+
+    const firebaseAuth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const loginWithGoogle = async () => {
+        await signInWithPopup(firebaseAuth, provider)
+            .then((userCred) => {
+                firebaseAuth.onAuthStateChanged((cred) => {
+                    cred.getIdToken().then((token) => {
+                        console.log(token);
+                    });
+                });
+            })
+            .catch((err) => {
+                console.log("eroare", err);
+            });
+    };
 
     return (
         <div className="w-screen h-screen relative flex overflow-hidden justify-center sm:justify-start">
@@ -119,6 +139,7 @@ function Login() {
                 <motion.div
                     {...buttonClick}
                     className="flex items-center justify-center px-20 py-2 bg-lightOverlay backdrop-blur-md cursor-pointer rounded-3xl gap-4"
+                    onClick={loginWithGoogle}
                 >
                     <FcGoogle className="text-3xl" />
                     <p className="capitalize text-base text-headingColor">
