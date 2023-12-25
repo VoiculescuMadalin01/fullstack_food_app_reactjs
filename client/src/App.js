ba4556a4a046756dc7d9a18376e0581ead3360c9
MADALIN_VOICULESCU <voiculescu.madalin1999@gmail.com~>
2023-12-25 11:34:54 +0200

    css loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import {Route, Routes} from "react-router-dom";
-// import Main from "./containers/Main";
 import {Main, Login} from "./containers";
 import {getAuth} from "firebase/auth";
 import {app} from "./config/firebase.config";
@@ -9,6 +8,7 @@ import {validateUserJWTToken} from "./api";
 import {setUserDetails} from "./context/actions/userActions";
 import {motion} from "framer-motion";
 import {fadeInOut} from "./animations";
+import {MainLoader} from "./components";
 
 const App = () => {
     const firebaseAuth = getAuth(app);
@@ -30,7 +30,7 @@ const App = () => {
                 }
                 setInterval(() => {
                     setIsLoading(false);
-                }, 3000);
+                }, 1600);
             });
         }
     }, []);
@@ -42,7 +42,7 @@ const App = () => {
                     {...fadeInOut}
                     className="fixed z-50 inset-0 bg-lightOverlay backdrop-blur-md flex items-center justify-center w-full"
                 >
-                    The data is loading!!!
+                    <MainLoader />
                 </motion.div>
             )}
             <Routes>
